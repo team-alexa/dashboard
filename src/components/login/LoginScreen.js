5d060676f5e2f333ec8cfbcd77794a9c37ddda9c
Mitchell Kossoris <mkossoris@gmail.com>
2019-02-11 18:19:07 -0600

    Added layout

---
+++ src/components/login/LoginScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../css/login/LoginScreen.css';
+
+class LoginScreen extends Component {
+  render() {
+    return (
+      <div className="login-screen">
+        <h1>This is the Login Screen</h1>
+      </div>
+    );
+  }
+}
+
+export default LoginScreen;
---
+++ src/components/main_app/Sidebar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../../css/main_app/Sidebar.css';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <div className={this.props.visible + " sidebar"}>
+        
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
