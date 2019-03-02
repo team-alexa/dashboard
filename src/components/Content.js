d039f468bbd4433cc7c692e26a129ba272dfcf52
Megan Waterworth <waterwo2@uwm.edu>
2019-03-01 21:44:36 -0600

    Update content to load AdminPage

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Home from './Home'
+import AdminPage from './AdminPage'
 import {DataConsumer} from '../Store'
 import '../css/Content.css';
 
 const components = {
   "": <Home />,
   "home": <Home />,
-  "profile": <Home />
+  "profile": <Home />,
+  "adminpanel": <AdminPage />
 }
 
 class Content extends Component {
